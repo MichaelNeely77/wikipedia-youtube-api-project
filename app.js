7b973b914989fec01c230d1967f7f2df9823f10a
Michael Neely <michael@michaelneely.net>
2020-05-18 00:57:35 -0400

    Added function of rendering search videos

--- app.js
+++ app.js
@@ -6,18 +6,31 @@ function showResults() {
     var searchTerm = document.querySelector('input[name="searchTerm"]').value;
     var url = 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&origin=*&search='+searchTerm;
     var output = document.querySelector('#output');
+    var outputYoutube = document.querySelector('#outputYoutube');
     output.innerHTML = "<h2>Search Term: "+searchTerm+"</h2>"
+    outputYoutube.innerHTML = "<h2>Youtube Videos for "+searchTerm+"</h2>"
     ajaxJS(url, function(response) {
         for (var x in response){
             var holder = typeof response[x] == 'string' ? response[x] : response[x][0];
             output.innerHTML += "<div class='dataOutput'>"+holder+"</div";
         }
     });
+    var url1 = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyCXJor9gOGxL06dbD6Iksn0fEHe_lH7hFs&q='+searchTerm+'&maxResults=10';
+    ajaxJS(url1, function(data) {
+        
+        for (var x in data.items){
+            console.log(data.items[x]);
+            var title = data.items[x].snippet.title;
+            var description = data.items[x].snippet.description;
+            var thumb = data.items[x].snippet.thumbnails.default.url;
+            var videoID = data.items[x].id.videoId;
 
-    // AIzaSyCXJor9gOGxL06dbD6Iksn0fEHe_lH7hFs
-    // https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyCXJor9gOGxL06dbD6Iksn0fEHe_lH7hFs&q=test&maxResults=10
-
-
+            
+            outputYoutube.innerHTML +=          
+            "<div class='vid-panel'><a href='https://youtu.be/"+videoID+"' target='_blank'><img src="+thumb+" alt=''></a><br>"+title+"</div>";
+            
+        } 
+    });
 }
 
 function ajaxJS(url, callback) {
